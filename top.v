8e2a06357122b01919ebbb97b4f2584aaebab87a
esarmah100 <37003572+esarmah100@users.noreply.github.com>
2021-03-27 15:40:07 -0500

    Create top.v

---
+++ top.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/27/2021 03:22:35 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input clk,
+    input [3:0] button,
+    input [1:0] sw,
+    
+    output [3:0] an,
+    output [6:0] seg
+    );
+    
+endmodule
