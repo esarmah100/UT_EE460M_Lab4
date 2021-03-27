872e7b4e93270bc5855ac39dffe92814d7044a93
esarmah100 <37003572+esarmah100@users.noreply.github.com>
2021-03-27 15:37:50 -0500

    Create meter.v

---
+++ meter.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/27/2021 03:22:16 PM
+// Design Name: 
+// Module Name: meter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module meter(
+    input clk,
+    input [3:0] button,
+    input [1:0] reset,
+    
+    output count
+    );
+     
+endmodule
